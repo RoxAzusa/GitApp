e1acdf589ec5223096b4a7b740c2740d6ebb28a2
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2023-09-27 14:51:20 +0200

    Modify the css of Layout

--- front/src/pages/Layout.js
+++ front/src/pages/Layout.js
@@ -4,13 +4,13 @@ import '../assets/css/style.css';
 const Layout = () => {
   return (
     <>    
-      <nav className="">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="nav">
+        <ul className="nav-item">
+          <li className="nav-button">
+            <Link className="nav-link" to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/Anime">Anime</Link>
+          <li className="nav-button">
+            <Link className="nav-link" to="/Anime">Anime</Link>
           </li>
         </ul>
       </nav>
