2b380467e91553b055094e6e98847c32adaa54f6
Cindy <101573729>
2023-09-27 14:57:01 +0200

    Update front

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Anime from "./pages/Anime";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
+import AddAnime from './pages/AddAnime';
+import Footer from './pages/Footer';
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="anime" element={<Anime />}/>
+            <Route path="/add" element={<AddAnime />} />
           </Routes>
+          <Footer />
       </BrowserRouter>
     </div>
   );
